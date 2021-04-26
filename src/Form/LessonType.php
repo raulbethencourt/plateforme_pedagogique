12650048e6796203d71baeece3f22559040c0136
Raul Bethencourt <raul.bethencourt.pro@gmail.com>
2021-04-26 12:48:57 +0200

    🚧 modification with Elise

--- src/Form/LessonType.php
+++ src/Form/LessonType.php
@@ -40,6 +40,7 @@ class LessonType extends AbstractType
                 CheckboxType::class,
                 [
                     'label' => 'Rendre visible',
+                    'required' => false,
                 ]
             )
             ->add(
@@ -47,6 +48,7 @@ class LessonType extends AbstractType
                 CheckboxType::class,
                 [
                     'label' => 'Rendre juable',
+                    'required' => false,
                 ]
             )
         ;
