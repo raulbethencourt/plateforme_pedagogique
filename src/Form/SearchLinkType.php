a38fed4e89759b53fa1987bff9e06093256bea5d
Raul Bethencourt <raul.bethencourt.pro@gmail.com>
2021-05-13 23:12:18 +0200

    🚧 make form and controller is done for searchlink

--- src/Form/SearchLinkType.php
+++ src/Form/SearchLinkType.php
@@ -2,8 +2,10 @@
 
 namespace App\Form;
 
-use App\Entity\SearchLink;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,16 +14,60 @@ class SearchLinkType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('text')
-            ->add('category')
-            ->add('author')
+            ->add(
+                'text',
+                TextType::class,
+                [
+                    'label' => false,
+                    'required' => false,
+                    'attr' => [
+                        'placeholder' => 'Titre...',
+                    ],
+                ]
+            )
+            ->add(
+                'category',
+                ChoiceType::class,
+                [
+                    'label' => false,
+                    'placeholder' => 'Categorie...',
+                    'choices' => [
+                        'Images' => 'images',
+                        'Pédagogie' => 'pedagogie',
+                        'Autres' => 'autres',
+                    ],
+                    'required' => false,
+                ]
+            )
+            ->add(
+                'author',
+                TextType::class,
+                [
+                    'label' => false,
+                    'required' => false,
+                    'attr' => [
+                        'placeholder' => 'Auteur·rice...',
+                    ],
+                ]
+            )
+            ->add(
+                'submit',
+                SubmitType::class,
+                [
+                    'label' => 'Chercher',
+                    'attr' => [
+                        'class' => 'btn btn-primary',
+                    ],
+                ]
+            )
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => SearchLink::class,
+            'method' => 'get',
+            'csrf_protection' => false,
         ]);
     }
 }
--- src/Repository/LinkRepository.php
+++ src/Repository/LinkRepository.php
@@ -3,9 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Link;
-use Doctrine\ORM\Query\Parameter;
 use Doctrine\Persistence\ManagerRegistry;
-use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
 /**
@@ -24,18 +22,17 @@ class LinkRepository extends ServiceEntityRepository
     /**
      * This method allows to find a link by creator or visibility.
      */
-    public function findByVisibilityOrCreator(bool $visibility, string $creator): array
+    public function findByVisibilityOrCreator(string $creator): array
     {
         return $this->createQueryBuilder('l')
-            ->where('l.visibility = :val1')
-            ->orWhere('l.creator = :val2')
-            ->setParameters(new ArrayCollection([
-                new Parameter('val1', $visibility),
-                new Parameter('val2', $creator),
-            ]))
+            ->where('l.visibility = 1')
+            ->orWhere('l.creator = :val')
+            ->setParameter('val', $creator)
             ->orderBy('l.id', 'ASC')
             ->getQuery()
             ->getResult()
         ;
     }
+
+    //TODO crear la requet pour search
 }
